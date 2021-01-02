39bf71999f928e5697e6eec63604397078aa4f61
vmagana3 <vmagana3@ucol.mx>
2021-01-01 20:43:33 -0600

    Section Contacto completed

--- scripts/coffeescripts.js
+++ scripts/coffeescripts.js
@@ -1,23 +1,3 @@
-
-//-----------------------------Show and hidde first element--------------------------------//
-var hiddenText = document.getElementById('hiddenText');
-
-hiddenText.addEventListener('click',showText)
-
-function showText()
-{
-    var text = this.nextElementSibling;
-    if(text.style.display === "none")
-    {        
-        text.style.display = "inline"                        
-    }   
-    else
-    {
-        text.style.display = "none"                  
-    } 
-
-}
-
 //-----------------------------Show and hidde second elements--------------------------------//
 var title = document.getElementsByClassName('misionTitle');
 for(var i = 0; i<title.length; i++)
